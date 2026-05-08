2187e6d5ed7b760362fa0f6656c79ce511684a1e
Albert Gil <agilmoreno@nvidia.com>
2026-05-08 12:57:09 +0200

    Testsuite - Improve coredump detection for t24822
    Sometimes the signature is slightly different.
    
    Ticket: 24822

--- testsuite/python/lib/atf.py
+++ testsuite/python/lib/atf.py
@@ -297,7 +297,6 @@ def classify_coredump(bin_path, bt_file, failures, xfailures):
     if (
         component in bin_path
         and "Program terminated with signal SIGABRT" in bt
-        and "OPENSSL_sk_free" in bt
         and "OPENSSL_cleanup" in bt
         and "_int_free_maybe_consolidate" in bt
     ):
